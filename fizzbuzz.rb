a096c910267cc1c1d31aa05dd6c279881ce605f0
Emma Konrad <emma.konrad92@gmail.com>
2017-09-30 23:41:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  end
+elsif in % 5 == 0
+    puts "Buzz"
+  end
+elsif in % 15 == 0
+    puts "Fizzbuss"
+  end
+else
+  puts " "
+end
 end
